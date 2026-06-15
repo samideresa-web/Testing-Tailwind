61681086323bdaad7942995cba8a1ae0eba98622
Your Name <yosefalemuone@gmail.com>
2026-06-15 14:18:49 +0300

    the hange to yosef

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div className='font-bold text-center text-blue-500'>
-      Hello World!
+      Hello World!!
     </div>
   )
 }
